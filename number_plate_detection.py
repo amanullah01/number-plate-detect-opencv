22eda29394bed07728d9b2265057ae0eca740766
Amanullah Aman <amancse.aiub@gmail.com>
2024-06-01 13:51:08 +0600

    if rameROI is none then do not show this

--- number_plate_detection.py
+++ number_plate_detection.py
@@ -3,21 +3,22 @@ import cv2
 video = cv2.VideoCapture('./videos/demo.mp4')
 
 numberPlateCascade = cv2.CascadeClassifier('./haarcascades/haarcascade_russian_plate_number.xml')
+# numberPlateCascade = cv2.CascadeClassifier('./haarcascades/BD_numberPlate_cascade_v1.xml')
 
 counter = 0
 
 while True:
     ret, frame = video.read()
-    frame = cv2.resize(frame, (640, 480))
-
     if not ret:
         break
 
+    frame = cv2.resize(frame, (640, 480))
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     number_plate = numberPlateCascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=10)
     print("enter this block")
     print("number plate:", number_plate)
     print("number plate length:", len(number_plate))
+    frameROI = None
     for (x, y, w, h) in number_plate:
         print(x, y, w, h)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -27,7 +28,8 @@ while True:
         frameROI = frame[y:y + h, x:x + w]
 
     cv2.imshow('frame', frame)
-    cv2.imshow('numberPlate', frameROI)
+    if frameROI is not None:
+        cv2.imshow('numberPlate', frameROI)
 
     # when press key 's' then it will take screenshot
     if cv2.waitKey(25) & 0xFF == ord('s'):
